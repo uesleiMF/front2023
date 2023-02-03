4caa0f651a2449124a40dcdc70c15180bd15050c
Ueslei <joseueslei2@gmail.com>
2023-02-02 22:08:09 -0300

    789

--- src/customHook/useRedirectLoggedOutUser.js
+++ src/customHook/useRedirectLoggedOutUser.js
@@ -1,5 +1,5 @@
 
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { SET_LOGIN } from "../redux/features/auth/authSlice";
